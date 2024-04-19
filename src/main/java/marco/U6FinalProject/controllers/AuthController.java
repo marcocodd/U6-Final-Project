9ed38eb415a59112d8a491d51217df13177c500b
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-19 12:58:59 +0200

    Added AuthController

--- src/main/java/marco/U6FinalProject/controllers/AuthController.java
+++ src/main/java/marco/U6FinalProject/controllers/AuthController.java
@@ -1,4 +1,38 @@
 package marco.U6FinalProject.controllers;
 
+import marco.U6FinalProject.exceptions.BadRequestException;
+import marco.U6FinalProject.payloads.NewUserDTO;
+import marco.U6FinalProject.payloads.NewUserResponseDTO;
+import marco.U6FinalProject.payloads.UserLoginDTO;
+import marco.U6FinalProject.payloads.UserLoginResponseDTO;
+import marco.U6FinalProject.services.AuthService;
+import marco.U6FinalProject.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private UsersService usersService;
+
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public NewUserResponseDTO save(@RequestBody @Validated NewUserDTO body, BindingResult validation) {
+        if (validation.hasErrors()) {
+            throw new BadRequestException(validation.getAllErrors());
+        }
+        return new NewUserResponseDTO(this.usersService.save(body).getId());
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDTO login(@RequestBody UserLoginDTO body) {
+        return new UserLoginResponseDTO(this.authService.authUserAndCreateToken(body));
+    }
 }
