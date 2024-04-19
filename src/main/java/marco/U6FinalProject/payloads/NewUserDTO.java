05a9c37ff61c4f16c30a7837bdec48171bc05c69
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-19 11:25:12 +0200

    NewUserDTO completed, added NewUserResponseDTO

--- src/main/java/marco/U6FinalProject/payloads/NewUserDTO.java
+++ src/main/java/marco/U6FinalProject/payloads/NewUserDTO.java
@@ -1,9 +1,18 @@
 package marco.U6FinalProject.payloads;
 
-public record NewUserDTO(String name,
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
-                         String surname,
+public record NewUserDTO(@NotEmpty(message = "Il nome è obbligatorio")
+                         @Size(min = 2, max = 20, message = " Il nome deve essere tra 2 e 20 caratteri") String name,
 
+                         @NotEmpty(message = "Il cognome è obbligatorio")
+                         @Size(min = 2, max = 20, message = "Il cognome deve essere tra 2 e 20 caratteri") String surname,
+
+                         @NotEmpty(message = "l'email è obbligatoria")
+                         @Email(message = "email inserita non valida")
                          String email,
-                         String password) {
+                         @NotEmpty(message = "La password è obbligatoria")
+                         @Size(min = 8, message = "La password deve essere di almeno 8 caratteri") String password) {
 }
---
+++ src/main/java/marco/U6FinalProject/payloads/NewUserResponseDTO.java
@@ -0,0 +1,4 @@
+package marco.U6FinalProject.payloads;
+
+public record NewUserResponseDTO(Long userId) {
+}
