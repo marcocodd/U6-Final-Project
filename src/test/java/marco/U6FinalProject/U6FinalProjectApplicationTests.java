e19d573fa74d40585f7dab6187710454f8bd862f
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-19 10:47:10 +0200

    Initial commit

---
+++ src/test/java/marco/U6FinalProject/U6FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package marco.U6FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U6FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
