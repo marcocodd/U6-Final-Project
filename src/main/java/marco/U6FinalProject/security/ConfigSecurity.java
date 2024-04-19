ebdd8ef1ad2e40463fe93fa80b2c0972a07bf72e
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-04-19 12:14:26 +0200

    Added configurationSecurity

--- src/main/java/marco/U6FinalProject/security/ConfigSecurity.java
+++ src/main/java/marco/U6FinalProject/security/ConfigSecurity.java
@@ -1,4 +1,48 @@
 package marco.U6FinalProject.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class ConfigSecurity {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin(http -> http.disable());
+        httpSecurity.csrf(http -> http.disable());
+        httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.cors(Customizer.withDefaults());
+        httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    PasswordEncoder getBCrypt() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        config.setAllowedOrigins(Arrays.asList("http://localhost:3001"));
+//        config.setAllowedMethods(Arrays.asList("*"));
+//        config.setAllowedHeaders(Arrays.asList("*"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//
+//        return source;
+//    }
 }
